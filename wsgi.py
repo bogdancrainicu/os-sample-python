9847570c510f253e1bc8bcbd65cff79d1984bd00
BC <cbogdan@upm.ro>
2016-11-19 01:09:45 +0200

    Gheorghe

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Traisca Vasile!"
+    return "Traisca Gheorghe!"
 
 if __name__ == "__main__":
     application.run()
